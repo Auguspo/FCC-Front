96c6042d15c64b6d2745b98b70e0fb3a457515a3
Auguspo <augustpolet@gmail.com>
2022-12-20 22:27:48 -0300

    Fix display

--- Drum Machine/main.js
+++ Drum Machine/main.js
@@ -1,15 +1,53 @@
-function play(name){
+function play(name) {
 
-    if (document.getElementById(name) !=null) {
+  if (document.getElementById(name) != null) {
     let audio = document.getElementById(name);
     console.log(name)
-    audio.play();}
+    audio.play();
+  }
+
+
 }
 
-window.document.onkeyup = function(event) {
-   play((event.key).toUpperCase())
- if (document.getElementById((event.key).toUpperCase()) !=null)
-  { let audio= document.getElementById((event.key).toUpperCase()).src.split("clip/").pop().split(".mp3")[0].replace(/%20/g, " ");
- document.getElementById("display").innerText = audio
-}}
+window.document.onkeyup = function (event) {
+  play((event.key).toUpperCase())
+
+  let text = ""
+  if (document.getElementById((event.key).toUpperCase()) != null)
+
+    switch ((event.key).toUpperCase()) {
+      case "Q":
+        text = "Heater 1"
+        break;
+      case "W":
+        text ="Heater 2"
+        break; 
+      case "E":
+        text ="Heater 3"
+        break;
+      case "A":
+        text ="Heater 4"
+        break;
+      case "S":
+        text ="Clap"
+        break;
+      case "D":
+        text = "Open HH"
+        break;
+      case "Z":
+        text ="Kick-n'-Hat"
+        break;
+      case "X":
+        text ="Kick"
+        break;
+      case "C":
+        text = "Closed HH"
+        break;
+        
+       default:
+        console.log("ERROR");
+    }
+
+  document.getElementById("display").innerText = text
+}
 
